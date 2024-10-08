73193b8ea6229a9764a3a7d1828fafbccca939e6
Mariana Torres <marianaptorres3@gmail.com>
2024-10-08 14:10:59 -0300

    Importa e inicia o banco de dados e testa a conexão

--- src/app.js
+++ src/app.js
@@ -1,2 +1,13 @@
-const express = require("express");
+import {openDb} from "./config/configDB.js";
+
+import express from 'express';
 const app = express();
+app.use(express.json());
+
+openDb();
+
+app.get('/', function(req,res){
+    res.send("hello world");
+})
+
+app.listen(3000, ()=> console.log("api rodando"));
