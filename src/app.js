9840c9477a33efc9ec4f277349dd449450e06cde
Mariana Torres <marianaptorres3@gmail.com>
2024-10-10 16:38:54 -0300

    Importa a função insertTarefa de inserção de dados para app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 //import {openDb} from "./config/configDB.js";
-import {createTable} from "../controller/tarefas.js";
+import {createTable, insertTarefa} from "../controller/tarefas.js";
 
 import express from 'express';
 const app = express();
 app.use(express.json());
 
+createTable();
+
 app.get('/', function(req,res){
     res.send("hello world");
 })
+
+app.post('/tarefa', function(req,res){
+    insertTarefa(req.body);
+    res.json({
+        "statusCode": 200
+    })
+});
 
 app.listen(3000, ()=> console.log("api rodando"));
